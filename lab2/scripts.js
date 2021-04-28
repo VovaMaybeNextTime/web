b26456985a5bacde1dc1d971e496b4733c3b1afd
VovaMaybeNextTime <48494728+VovaMaybeNextTime@users.noreply.github.com>
2021-04-28 10:47:21 +0300

    Add files via upload

---
+++ lab2/scripts.js
@@ -0,0 +1,65 @@
+
+function displayLab1()
+{
+    document.getElementById("lab-list1").hidden = false;
+    document.getElementById("lab-list2").hidden = true;
+    document.getElementById("lab-list3").hidden = true;
+    document.getElementById("lab-list4").hidden = true;
+    document.getElementById("lab-list5").hidden = true;
+    document.getElementById("lab-list6").hidden = true;
+}
+
+function displayLab2()
+{
+    document.getElementById("lab-list1").hidden = true;
+    document.getElementById("lab-list2").hidden = false;
+    document.getElementById("lab-list3").hidden = true;
+    document.getElementById("lab-list4").hidden = true;
+    document.getElementById("lab-list5").hidden = true;
+    document.getElementById("lab-list6").hidden = true;
+}
+
+function displayLab3()
+{
+    document.getElementById("lab-list1").hidden = true;
+    document.getElementById("lab-list2").hidden = true;
+    document.getElementById("lab-list3").hidden = false;
+    document.getElementById("lab-list4").hidden = true;
+    document.getElementById("lab-list5").hidden = true;
+    document.getElementById("lab-list6").hidden = true;
+}
+function displayLab4()
+{
+    document.getElementById("lab-list1").hidden = true;
+    document.getElementById("lab-list2").hidden = true;
+    document.getElementById("lab-list3").hidden = true;
+    document.getElementById("lab-list4").hidden = false;
+    document.getElementById("lab-list5").hidden = true;
+    document.getElementById("lab-list6").hidden = true;
+}
+function displayLab5()
+{
+    document.getElementById("lab-list1").hidden = true;
+    document.getElementById("lab-list2").hidden = true;
+    document.getElementById("lab-list3").hidden = true;
+    document.getElementById("lab-list4").hidden = true;
+    document.getElementById("lab-list5").hidden = false;
+    document.getElementById("lab-list6").hidden = true;
+}
+function displayLab6()
+{
+    document.getElementById("lab-list1").hidden = true;
+    document.getElementById("lab-list2").hidden = true;
+    document.getElementById("lab-list3").hidden = true;
+    document.getElementById("lab-list4").hidden = true;
+    document.getElementById("lab-list5").hidden = true;
+    document.getElementById("lab-list6").hidden = false;
+}
+
+
+
+$(document).ready(function(){ document.getElementById("lab-list2").hidden = true; }) 
+$(document).ready(function(){ document.getElementById("lab-list3").hidden = true; })
+$(document).ready(function(){ document.getElementById("lab-list4").hidden = true; })
+$(document).ready(function(){ document.getElementById("lab-list5").hidden = true; })
+$(document).ready(function(){ document.getElementById("lab-list6").hidden = true; })
